450083e420827a5b0f95293e9be391975918ecd2
vuju-bc <vuju@bc.edu>
2022-09-12 10:34:51 -0400

    Committ #2 9/12/22

---
+++ JulieVuProject1.R
@@ -0,0 +1,34 @@
+library(ggplot2)
+library(tidyverse)
+setwd("~/Desktop/Exp Methods/Files")
+dat <- read.csv("scales.csv")
+dim(dat)
+sapply(dat,class)
+dat$species <- as.factor(dat$species)
+species <- levels(dat$species)
+species
+species.n<- dat %>%
+  group_by(species) %>%
+  summarise(n = n())
+species.n
+dat %>% 
+  count(species,specimen) %>%
+  print() %>%
+  count(species,name = "n.specimens")
+for(i in 1:10) print(i)
+for(i in species){
+  p <- dat %>%
+    filter(species==i)%>%
+    ggplot()+geom_boxplot(aes(x=quadrant,y=N))+ggtitle(i)
+  print(p)
+}
+pdf("species.quadrant.pdf")
+for(i in species){
+  p <- dat %>%
+    filter(species==i)%>%
+    ggplot()+geom_boxplot(aes(x=quadrant,y=N))+ggtitle(i)
+  print(p)
+}
+dev.off()
+list.files(pattern=".pdf")
+
